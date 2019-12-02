1bfffbe4595fce0fd82dfc77132b34c477ad71ed
Natthapong None Choeypant <choeypn@cf405-06.cs.wwu.edu>
2019-12-02 13:39:51 -0800

    doing Jacobi iteration

--- thread.c
+++ thread.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "thread.h"
 
 int main(int argc, char *argv[]){
   double in;
@@ -17,13 +18,32 @@ int main(int argc, char *argv[]){
     j = 0;
     while(j < 1024){
       M[i][j] = in;
-      //printf("M[%d][%d] = %.10lf \n",i,j,M[i][j]);
+      if(i == 0 || i == 1023)
+        N[i][j] = in;
+      else if(j == 0 || j == 1023)
+        N[i][j] = in;
+      //printf("N[%d][%d] = %.10lf \n",i,j,N[i][j]);
       fscanf(f,"%lf",&in);
-      ++j;
+      j++;
     }
   }
+  jacobi(M,N);
+
   if(f != stdin)
     fclose(f);
 
   return 0;
+}
+
+void jacobi(double (*M)[1024],double (*N)[1024]){
+ 
+  for(int i = 1; i < 1022;i++){
+    for(int j = 1; j < 1022;j++){
+      N[i][j] = (M[i-1][j] + M[i+1][j] + M[i][j-1] + M[i][j+1])/4.0;
+    }
+  }
+/*
+  for(int k = 0; k < 1023;k++)
+    printf("N[1][%d] : %.10lf \n",k,N[1][k]);
+*/
 }
---
+++ thread.h
@@ -0,0 +1,7 @@
+/*
+  CSCI 347 Assignment#4
+
+*/
+
+
+void jacobi(double (*M)[1024], double (*N)[1024]);
