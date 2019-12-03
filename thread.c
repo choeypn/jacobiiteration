7a84be54ca8639624d1768021e37d77b1c656cbe
Natthapong None Choeypant <choeypn@cf405-09.cs.wwu.edu>
2019-12-02 16:57:05 -0800

    incomplete termination point for Jacobi Iteration

--- thread.c
+++ thread.c
@@ -33,22 +33,24 @@ int main(int argc, char *argv[]){
 }
 
 void jacobi(double (*M)[1024],double (*N)[1024]){
- double old = 0, new = 1, eps;
- int k = 0, state = 1; 
- eps = 0.001;
- while(k < 200){ 
+ double old = 0, new, eps;
+ int k = 0, state; 
+ eps = 0.002;
+ while(k < 1000){
+  state = 1; 
   for(int i = 1; i < 1023;i++){
     for(int j = 1; j < 1023;j++){ 
       new = (N[i-1][j] + N[i+1][j] + N[i][j-1] + N[i][j+1])/4.0;
-      if(new-N[i][j] > eps){
+      if(!new-N[i][j] < eps){
         N[i][j] = new;
+        state = 0;
       }
-      //N[i][j] = (N[i-1][j] + N[i+1][j] + N[i][j-1] + N[i][j+1])/4.0;
     }
   }
-  
-  k++;
+ if(state)
+   break;
+ k++;
  }
   //for(int k = 0; k < 1023;k++)
-    printf("N[1][1] : %.10lf \n",N[1][1]);
+    printf("N[101][102] : %.10lf \n",N[101][102]);
 }
