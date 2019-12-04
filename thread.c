c78233b69c53151db1de3c4e9bed091488cc3171
Natthapong None Choeypant <choeypn@cf405-05.cs.wwu.edu>
2019-12-03 17:49:39 -0800

    one thread now working on Jacobi, makefile couldn't compile thread (use gcc -o thread thread.c -lpthread)

--- thread.c
+++ thread.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
+#include <time.h>
+#include <pthread.h>
 #include "thread.h"
 
 int main(int argc, char *argv[]){
   double in;
-  int j;
+  int j,start,end;
   FILE *f;
   double(*M)[1024] = malloc(sizeof(double)*1024*1024);
   double(*N)[1024] = malloc(sizeof(double)*1024*1024);
@@ -24,36 +26,64 @@ int main(int argc, char *argv[]){
       j++;
     }
   }
-  jacobi(M,N);
-
+  
+  clock_gettime(CLOCK_REALTIME,&TIMESPEC);
+  start = TIMESPEC.tv_sec;
+  
+  jacobi(M,N);  
+   
+  clock_gettime(CLOCK_REALTIME,&TIMESPEC);
+  end = TIMESPEC.tv_sec;
+  printf("%d - %d = %d \n",start,end,end-start);
   if(f != stdin)
     fclose(f);
-
   return 0;
 }
 
+/* 
+  Jacobi iteration function 
+  Read matrix M and assign values to N.
+  Keep iterating until every element is in stable state.
+*/
 void jacobi(double (*M)[1024],double (*N)[1024]){
- double old = 0, new, eps;
- int done = 0; 
- eps = 0.001;
- while(!done){
-  done = 1; 
-  for(int i = 1; i < 1023;i++){
-    for(int j = 1; j < 1023;j++){
-      N[i][j] = (M[i-1][j] + M[i+1][j] + M[i][j-1] + M[i][j+1])/4.0;
-    } 
-  }
 
-  for(int i = 1; i < 1023;i++){
-    for(int j = 1; j < 1023;j++){
-      if(fabs(M[i][j] - N[i][j]) > eps){
-        done = 0;
-      }
-      M[i][j] = N[i][j];
-    } 
+ pthread_t mythread;
+ struct threadArgs thdargs;
+ thdargs.M = M;
+ thdargs.N = N;
+ if(pthread_create(&mythread,NULL,thdJacobi,&thdargs))
+   perror("thread error");
+
+ void *p; 
+ if(pthread_join(mythread,&p))
+   perror("join error"); 
+
+}
+
+void *thdJacobi(void *arg){
+  struct threadArgs *p = arg;
+  int done = 0;
+  double eps = 0.001;
+  double(*m)[1024] = p->M;
+  double(*n)[1024] = p->N; 
+
+  while(!done){
+    done = 1; 
+    for(int i = 1; i < 1023;i++){
+      for(int j = 1; j < 1023;j++){
+        n[i][j] = (m[i-1][j] + m[i+1][j] + m[i][j-1] + m[i][j+1])/4.0;
+      } 
+    }
+
+    for(int i = 1; i < 1023;i++){
+      for(int j = 1; j < 1023;j++){
+        if(fabs(m[i][j] - n[i][j]) > eps){
+          done = 0;
+        }
+        m[i][j] = n[i][j];
+      } 
+    }
   }
- }
- int l = 40;
- int k = 500;
- printf("N[%d][%d] : %.10lf \n",l,k,N[l][k]);
+  puts("thread finished processing");
+  return p;
 }
--- thread.h
+++ thread.h
@@ -3,5 +3,14 @@
 
 */
 
+struct threadArgs {
+  int tnum;
+  double(*M)[1024];
+  double(*N)[1024];
+};
+
+struct timespec TIMESPEC;
 
 void jacobi(double (*M)[1024], double (*N)[1024]);
+
+void *thdJacobi(void *arg);
